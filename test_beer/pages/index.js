5ddf20cd3c36cce9923b94736e5c3273863e3417
Zheka <Zh3kaSel@yandex.ru>
2022-07-28 20:53:22 +0300

    (feat): Get cards of api

--- test_beer/pages/index.js
+++ test_beer/pages/index.js
@@ -1,11 +1,54 @@
-import styles from '../styles/Home.module.scss'
+import React, { useEffect, useState } from "react";
+import styles from "../styles/Home.module.scss";
+import axios from "axios";
+import Beer from "../components/Beer/Beer";
 
-const Home = () => {
+const Home = ({ beers }) => {
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const filteredBeers = beers.filter((beer) =>
+    beer.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className={styles.container}>
-      some text
+      <header>
+        <form>
+          <h1>Search a beer</h1>
+          <input
+            type="text"
+            placeholder="Search a beer"
+            className={styles.search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </form>
+      </header>
+      <div className={styles.beersCont}>
+        {filteredBeers.map((beer) => {
+          return (
+            <Beer
+              key={beer.id}
+              name={beer.name}
+              description={beer.description}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://api.punkapi.com/v2/beers");
+  const data = await res.json();
+
+  return {
+    props: {
+      beers: data,
+    },
+  };
+};
